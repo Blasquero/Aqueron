c4360c8da731fbc07353947a2a2af2a22a6b4d0d
André <andrescrich@hotmail.com>
2019-03-13 03:03:25 +0100

    Prototipo gancho
    Cuando pulsas click izquierdo, lanza la guadaña y al impactar con ground desplaza a eva hasta la posicion de la guadaña

---
+++ Aqueron/Assets/EvaGanchoScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvaGanchoScript : MonoBehaviour
+{
+    public GameObject gancho;
+    public Transform ganchoTrans;
+    private bool attackDone;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && !attackDone)
+        {
+            Instantiate(gancho, ganchoTrans.position, ganchoTrans.rotation);
+            attackDone = true;
+            Invoke("AttackDone", 0.5f);
+        }
+    }
+    
+    void AttackDone() {
+        attackDone = false;
+    }
+
+}
---
+++ Aqueron/Assets/GanchoScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GanchoScript : MonoBehaviour
+{
+
+    private GameObject player;
+    private Rigidbody2D rb;
+    private int groundLayer;
+    private bool ganchoActivo;
+    private bool deberiaMoverse;
+    private bool hit;
+    public PhysicsMaterial2D stickyMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        groundLayer = LayerMask.NameToLayer("Ground");
+        Invoke("AutoDestruccion", 0.25f);
+        Physics2D.IgnoreLayerCollision(12, 9);
+        Physics2D.IgnoreLayerCollision(12, 10);
+        rb = GetComponent<Rigidbody2D>();
+        deberiaMoverse = true;
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (ganchoActivo)
+        {
+            player.transform.position = Vector2.MoveTowards(player.transform.position, transform.position, 30f * Time.fixedDeltaTime);
+            player.GetComponent<Rigidbody2D>().gravityScale = 0;
+
+        }
+    }
+    private void Update()
+    {
+        if (Mathf.Abs(player.transform.position.x - transform.position.x) < 1 &&
+            Mathf.Abs(player.transform.position.y - transform.position.y) < 1)
+        {
+            Destroy(gameObject);
+            player.GetComponent<Rigidbody2D>().gravityScale = 7;
+            player.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        }
+        if(deberiaMoverse)
+        transform.Translate(new Vector2(0f, 20f) * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == groundLayer)
+        {
+            ganchoActivo = true;
+            transform.Translate(Vector3.zero);
+            deberiaMoverse = false;
+            hit = true;
+        }
+    }
+
+    void AutoDestruccion()
+    {
+        if(!hit)
+        Destroy(gameObject);
+
+        player.GetComponent<Rigidbody2D>().gravityScale = 7;
+    }
+}
