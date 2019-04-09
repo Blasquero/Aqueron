ce48f38f260db0639382f97db1d2f2b8be821d17
André <andrescrich@hotmail.com>
2019-04-09 16:29:37 +0200

    Updates Minimapa

---
+++ Aqueron/Assets/MiniMapScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapScript : MonoBehaviour
+{
+    private Animator animator;
+    private bool MapMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MapMenu = false;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Mapa") && !MapMenu)
+        {
+            animator.enabled = true;
+            animator.SetBool("MapaMenu", true);
+        }
+        if (Input.GetButtonDown("Mapa") && MapMenu)  
+        {
+            animator.SetBool("MapaMenu", false);
+        }
+    }
+
+    public void CambioAMapMenu()
+    {
+        MapMenu = !MapMenu;
+    }
+}
